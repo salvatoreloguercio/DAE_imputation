43236d5473c4c95eb07d79046f219f04f536b041
salvatoreloguercio <salvatore.loguercio@gmail.com>
2023-05-15 16:50:13 -0700

    transfer - fix recov tiles

--- II_testing/transfer_dropbox2hpc_recov.sh
+++ II_testing/transfer_dropbox2hpc_recov.sh
@@ -8,6 +8,9 @@ ls -l rclone_logs | awk '{if ($5 != 0) print $9}' | sed -e "s/_rclone.log//g" | 
 echo -e "Tile\tBest_model\tRank" > tiles_recovered.txt
 
 while read tile_err; do
+
+	tile_prefix=$(echo ${tile_err} | cut -d '/' -f2)
+
     echo $tile_err
 	chr=$(echo $tile_err | cut -d '_' -f1)
   
@@ -25,6 +28,7 @@ while read tile_err; do
     done
     cd /mnt/stsi/stsi6/Internal/INCITE/II_testing/chr$chr/$tile_err
     rclone copy dropbox:Torkamani_Lab/archive/INCITE/chr${chr}/${tile_err}/IMPUTATOR${best_trial} IMPUTATOR${best_trial} --transfers 5 --dropbox-chunk-size 128M --log-file=/mnt/stsi/stsi6/Internal/INCITE/II_testing/rclone_logs_recov/${tile_prefix}_rclone.log
+    echo "${tile_err}\t${best_trial}\t$i"
     echo -e "${tile_err}\t${best_trial}\t$i" >> tiles_recovered.txt
   fi
 
